73914e9b65563090238fef96e060c1aff8aa805f
rishat <rishat.mansurov@rt.ru>
2023-03-30 18:35:29 +0300

    Initial commit

---
+++ src/main/java/com/example/reactivekafkaconsumer/CaseDto.java
@@ -0,0 +1,20 @@
+package com.example.reactivekafkaproducer.dtos;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CaseDto {
+
+    private String number;
+    private String description;
+    private LocalDateTime startTime;
+    List<CaseLogDto> caseLogDtoList;
+}
---
+++ src/test/java/com/example/reactivekafkaconsumer/ReactiveKafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactivekafkaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveKafkaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
